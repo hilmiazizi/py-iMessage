5fc1a09d8175b6ec772813bab0789938e12fa375
Hilmi <hilmiazizi19@gmail.com>
2020-06-12 18:32:26 +0700

    Update imessage.py

--- py_imessage/imessage.py
+++ py_imessage/imessage.py
@@ -1,40 +1,44 @@
-from py_imessage import imessage
+from py_imessage import db_conn
 import os
-import sys
+import subprocess
+from time import sleep
+from shlex import quote
 
 
-###
-message = "Your message go here"
-###
+def send(phone, message):
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    relative_path = 'osascript/send_message.js'
+    path = f'{dir_path}/{relative_path}'
+    cmd = f'osascript -l JavaScript {path} {quote(phone)} {quote(message)}'
+    subprocess.call(cmd, shell=True)
 
-os.system('clear')
-def doDelete(phone):
-	payload = "sed -i '' '/"+phone+"/d' ./"+sys.argv[1]
-	os.system(payload)
+    # Get chat message db that was sent (look at most recently sent to a phone number)
+    db_conn.open()
+    
+    # Option 1: Loop until result is valid (hard to determine validity without adding other info to the DB)
+    # Option 2: Sleep for 1 sec to allow local db to update :((
+    sleep(1)
+    guid = db_conn.get_most_recently_sent_text()
+    return guid
 
-def doSend(phone):
-	global counter
-	global num_lines
-	global message
-	counter = counter+1
-	if not imessage.check_compatibility(phone):
-		print("["+str(counter)+"|"+str(num_lines)+"] Failed  => "+phone+" | Not an iPhone")
-		doDelete(phone)
-		return None
-	try:
-		guid = imessage.send(phone, message)
-	except Exception as e:
-		doDelete(phone)
-		if 'unable to open database file' in str(e):
-			print("["+str(counter)+"|"+str(num_lines)+"] Success => "+phone)
-		else:
-			return None
 
-counter = 0
-num_lines = sum(1 for line in open(sys.argv[1]))
-for line in open(sys.argv[1]):
-	if '+' in line:
-		print(phone+' => Wrong format!')
-		pass
-	else:
-		doSend(line.rstrip())
+def status(guid):
+    db_conn.open()
+    message = db_conn.get_message(guid)
+    return message
+
+
+def check_compatibility(phone):
+    is_imessage = False
+    
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    relative_path = 'osascript/check_imessage.js'
+    path = f'{dir_path}/{relative_path}'
+    cmd = f'osascript -l JavaScript {path} {phone}'
+    # Gets all the output from the imessage
+    output = subprocess.check_output(cmd, shell=True)
+
+    if 'true' in output.decode('utf-8'):
+        is_imessage = True
+
+    return is_imessage
